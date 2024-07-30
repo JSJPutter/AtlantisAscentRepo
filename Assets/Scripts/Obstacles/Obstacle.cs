8514c636e6c3844f3c2425b7ef1625519a1655d7
Dmitrijs Zubriks <dima.zubrik@gmail.com>
2024-07-30 12:33:29 +0000

    Create Obstacle base class

---
+++ Assets/Scripts/Obstacles/Obstacle.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+public abstract class Obstacle : MonoBehaviour
+{
+    public int hitPoints = 1;
+    public bool isDestructible = false;
+    public float sinkForce = 0.3f;
+    public float horizontalWaveMagnitude = 0.5f;
+    public float horizontalWaveFrequency = 1f;
+
+    protected Rigidbody2D rb;
+    private float startX;
+    private float timeSinceSpawn;
+
+    protected virtual void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            rb = gameObject.AddComponent<Rigidbody2D>();
+        }
+        rb.gravityScale = 1;
+        startX = transform.position.x;
+        timeSinceSpawn = 0f;
+    }
+
+    protected virtual void Update()
+    {
+        if (IsOutOfView())
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        ApplyBuoyantBehavior();
+    }
+
+    protected void ApplyBuoyantBehavior()
+    {
+        timeSinceSpawn += Time.fixedDeltaTime;
+
+        rb.AddForce(Vector2.down * sinkForce);
+
+        float horizontalOffset = Mathf.Sin(timeSinceSpawn * horizontalWaveFrequency) * horizontalWaveMagnitude;
+        Vector2 horizontalForce = new Vector2(horizontalOffset - (rb.position.x - startX), 0);
+        rb.AddForce(horizontalForce);
+    }
+
+    protected bool IsOutOfView()
+    {
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
+        return screenPoint.y < 0 || screenPoint.x < 0;
+    }
+
+    public virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                player.TakeDamage(1);
+            }
+            if (isDestructible)
+            {
+                TakeDamage(1);
+            }
+        }
+    }
+
+    public virtual void TakeDamage(int damage)
+    {
+        if (isDestructible)
+        {
+            hitPoints -= damage;
+            if (hitPoints <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
