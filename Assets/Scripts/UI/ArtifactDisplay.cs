ca9c87bc6cfe94afb8f904313ff8488aff634b93
Dmitrijs Zubriks <dima.zubrik@gmail.com>
2024-08-12 14:43:09 +0000

    Implement artifact system for collection and upgrades

---
+++ Assets/Scripts/UI/ArtifactDisplay.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using Artifacts;
+
+public class ArtifactDisplay : MonoBehaviour
+{
+    public static ArtifactDisplay Instance { get; private set; }
+
+    public GameObject artifactIconPrefab;
+    public Transform artifactContainer;
+
+    private List<GameObject> artifactIcons = new List<GameObject>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void UpdateDisplay()
+    {
+        // Clear existing icons
+        foreach (var icon in artifactIcons)
+        {
+            Destroy(icon);
+        }
+        artifactIcons.Clear();
+
+        foreach (var artifact in ArtifactManager.Instance.collectedArtifacts)
+        {
+            GameObject newIcon = Instantiate(artifactIconPrefab, artifactContainer);
+            newIcon.GetComponent<Image>().sprite = artifact.icon;
+            artifactIcons.Add(newIcon);
+        }
+    }
+}
