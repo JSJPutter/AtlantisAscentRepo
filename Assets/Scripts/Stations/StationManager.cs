38b011378c64fcc38151957f57f26659fce12d41
Dmitrijs Zubriks <dima.zubrik@gmail.com>
2024-08-22 11:43:22 +0000

    Add missing stations

---
+++ Assets/Scripts/Stations/StationManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
