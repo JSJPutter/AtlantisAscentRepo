d2c58b0560e23e5fbd48609d7171898db537071a
Dmitrijs Zubriks <dima.zubrik@gmail.com>
2024-09-04 20:43:34 +0300

    Add upgrades scene and code

---
+++ Assets/ScriptableObjects/Upgrades/Upgrade.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Upgrade", menuName = "Upgrades/Upgrade")]
+public class UpgradeData : ScriptableObject
+{
+    public string upgradeName;
+    public string description;
+    public int maxLevel;
+    public int baseCost;
+    public float costMultiplier;
+    public Sprite icon;
+
+    public virtual void ApplyUpgrade(PlayerController player, int level)
+    {
+        // Override this method in derived classes to apply specific upgrades
+    }
+}
+
+// Example of a specific upgrade type
+[CreateAssetMenu(fileName = "New Max Health Upgrade", menuName = "Upgrades/Max Health Upgrade")]
+public class MaxHealthUpgrade : UpgradeData
+{
+    public int healthIncreasePerLevel = 10;
+
+    public override void ApplyUpgrade(PlayerController player, int level)
+    {
+        player.maxHealth += healthIncreasePerLevel * level;
+    }
+}
+
+// Another example
+[CreateAssetMenu(fileName = "New Move Speed Upgrade", menuName = "Upgrades/Move Speed Upgrade")]
+public class MoveSpeedUpgrade : UpgradeData
+{
+    public float speedIncreasePerLevel = 5f;
+
+    public override void ApplyUpgrade(PlayerController player, int level)
+    {
+        player.moveSpeed += speedIncreasePerLevel * level;
+    }
+}
